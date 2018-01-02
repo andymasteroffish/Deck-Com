ed04df048441ea259119e06ecbcb2c32b1e47b49
Andy Wallace <andymasteroffish@gmail.com>
2018-01-01 21:08:29 -0500

    some buttons pulse to draw attention to themselves

--- deck-com/Assets/scripts/monobehaviors/GenericButton.cs
+++ deck-com/Assets/scripts/monobehaviors/GenericButton.cs
@@ -13,7 +13,10 @@ public class GenericButton : MonoBehaviour {
 	public Color normalColor = Color.white;
 	public Color mouseOverColor = new Color (0.75f, 0.75f, 0.75f);
 
-	//public bool doNotForceMouseIsOver;
+	//some dumb placeholder effects to make UI more obvious 
+	public bool pulseWhenPlayerExausted;
+	public bool pulseAllTheTime;
+
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +26,15 @@ public class GenericButton : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		frameRend.color = mouseIsOver ? mouseOverColor : normalColor;
+
+		bool isPulsing = pulseAllTheTime;
+		if (pulseWhenPlayerExausted) {
+			isPulsing = GameManagerTacticsInterface.instance.gm.areAllPlayerUnitsExausted ();
+		}
+		if (isPulsing) {
+			float newScale =  1.1f + Mathf.Sin(Time.time*2) * 0.25f;
+			transform.localScale = new Vector3(newScale, newScale, 1);
+		}
 
 		mouseIsOver = false;	//this will be overwritten by onMouseOver if they are on the button
 	}
@@ -35,13 +47,6 @@ public class GenericButton : MonoBehaviour {
 		target.SendMessage (message);
 		mouseIsOver = false;
 	}
-
-//	void OnMouseEnter(){
-//		mouseIsOver = true;
-//	}
-//	void OnMouseExit(){
-//		mouseIsOver = false;
-//	}
 
 
 
